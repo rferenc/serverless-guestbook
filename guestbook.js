5c96a9a2c2b44540d92d52adde3edeb8b0459976
rferenc <franky@mozdit.hu>
2021-04-14 09:47:12 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://0ca65c37.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
